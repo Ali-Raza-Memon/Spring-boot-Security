db7e1ec99aaf040e2f964392153c392f0d549cd3
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2023-06-16 00:54:46 +0500

    Database is not connected and there is static data saved in array list

--- src/main/java/com/spring/security/controller/UserControl.java
+++ src/main/java/com/spring/security/controller/UserControl.java
@@ -1,4 +1,27 @@
 package com.spring.security.controller;
 
+
+import com.spring.security.model.User;
+import com.spring.security.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
 public class UserControl {
+
+    @Autowired
+    private UserService userService;
+
+    //all users
+    @GetMapping("/allUsers")
+    public List<User> getAllUsers(){
+        return userService.getAllUsers();
+    }
+
+
 }

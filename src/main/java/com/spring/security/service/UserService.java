5a738778451212ba3502ddd59c75ffb3dd7ae1e0
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2023-06-16 00:48:56 +0500

    Three methods are created in Service layer

--- src/main/java/com/spring/security/service/UserService.java
+++ src/main/java/com/spring/security/service/UserService.java
@@ -26,8 +26,12 @@ public class UserService {
     }
 
     public User getUser(String username){
-        return this.list.stream().filter((user)->user.getUsername().equals(username)).findAny().get();
+        return this.list.stream().filter((user)->user.getUsername().equals(username)).findAny().orElse(null);
     }
 
+    public User addUser(User user){
+        this.list.add(user);
+        return user;
+    }
 
 }

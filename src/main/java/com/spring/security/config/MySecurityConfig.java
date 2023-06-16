634a546d6d635c566029a766ad5c9dcce5a6ebf3
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2023-06-16 10:41:57 +0500

    One Normal User and Admin are created but there is an error of encoder

--- src/main/java/com/spring/security/config/MySecurityConfig.java
+++ src/main/java/com/spring/security/config/MySecurityConfig.java
@@ -1,6 +1,7 @@
 package com.spring.security.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -22,4 +23,9 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("Ali").password("memon").roles("NORMAL");
+        auth.inMemoryAuthentication().withUser("Muzammil").password("Ali").roles("ADMIN");
+    }
 }

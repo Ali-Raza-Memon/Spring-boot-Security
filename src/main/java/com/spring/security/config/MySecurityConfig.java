efd81e0dedd39359cf406c5d5fe32237f5a2e2db
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2023-06-16 11:25:01 +0500

    Now there we have one public URL and throw it we can access all URLs

--- src/main/java/com/spring/security/config/MySecurityConfig.java
+++ src/main/java/com/spring/security/config/MySecurityConfig.java
@@ -20,7 +20,7 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/home","/login","/register").permitAll()
+                .antMatchers("/public/**").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()

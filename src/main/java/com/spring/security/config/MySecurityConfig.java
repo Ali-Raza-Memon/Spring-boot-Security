95d40339c4ee4c9f721cccc633cd8a2aeab06949
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2023-06-16 10:46:22 +0500

    Error resolved! Added method PasswordEncoder

--- src/main/java/com/spring/security/config/MySecurityConfig.java
+++ src/main/java/com/spring/security/config/MySecurityConfig.java
@@ -1,10 +1,13 @@
 package com.spring.security.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @Configuration
@@ -28,4 +31,13 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication().withUser("Ali").password("memon").roles("NORMAL");
         auth.inMemoryAuthentication().withUser("Muzammil").password("Ali").roles("ADMIN");
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+
+
+
 }

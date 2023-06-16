da981ab6b92076306b34fd4aaaa04a502c357513
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2023-06-16 10:54:24 +0500

    Now password will be encoded first and then saved in database

--- src/main/java/com/spring/security/config/MySecurityConfig.java
+++ src/main/java/com/spring/security/config/MySecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -28,13 +29,13 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("Ali").password("memon").roles("NORMAL");
-        auth.inMemoryAuthentication().withUser("Muzammil").password("Ali").roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("Ali").password(this.passwordEncoder().encode("memon")).roles("NORMAL");
+        auth.inMemoryAuthentication().withUser("Muzammil").password(this.passwordEncoder().encode("Ali")).roles("ADMIN");
     }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder(10);
     }
 
 
